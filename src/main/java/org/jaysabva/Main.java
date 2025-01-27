b285eabf5812e3ce3f4b3d24e0114155f75f520a
JaySabva-Ontic <jsabva@ontic.co>
2025-01-27 16:46:20 +0530

    Decoupled Code to align with new file structure (Controller, Service, Repository)

--- src/main/java/org/jaysabva/Main.java
+++ src/main/java/org/jaysabva/Main.java
@@ -1,15 +1,20 @@
 package org.jaysabva;
 
-import java.time.LocalDate;
+import org.jaysabva.controller.TaskContoller;
+import org.jaysabva.controller.UserController;
+import org.jaysabva.entity.*;
+
 import java.time.LocalDateTime;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-        List<Task> tasks = new ArrayList<>();
-        users.add(createAdmin());
-        users.add(createUser());
+        UserController userController = new UserController();
+        TaskContoller taskContoller = new TaskContoller();
+
+        userController.registerUser("admin", "admin", "admin");
+        userController.registerUser("user", "user", "user");
+
 
         Scanner scanner = new Scanner(System.in);
 
@@ -18,12 +23,13 @@ public class Main {
             User loggedInUser = null;
             while (true) {
                 try {
-                    loggedInUser = login(users, scanner);
+                    String[] loginInput = loginInput(scanner);
+                    loggedInUser = userController.loginUser(loginInput[0], loginInput[1]);
                     if (loggedInUser != null) {
                         System.out.println("Login successful!");
                         break;
                     } else {
-                        System.out.println("Login failed! Retry.");
+                        System.out.println("Invalid credentials. Please try again.");
                     }
                 } catch (InputMismatchException e) {
                     System.out.println("Invalid input. Please enter a valid option.");
@@ -31,7 +37,7 @@ public class Main {
                 }
             }
 
-            if (loggedInUser != null && loggedInUser.getRole().equals("admin")) {
+            if (loggedInUser.getRole().equals("admin")) {
                 while (true) {
                     try {
                         System.out.print("1 - Create User\n" +
@@ -44,53 +50,39 @@ public class Main {
                         int option = Integer.parseInt(scanner.nextLine());
 
                         boolean taskRelated = false;
+                        String[] signUpInput;
+                        String username;
 
                         switch (option) {
                             case 1:
-                                users.add(createUser(scanner, (long) users.size() + 1));
+                                signUpInput = signUpInput(scanner);
+                                userController.registerUser(signUpInput[0], signUpInput[1], signUpInput[2]);
+
                                 break;
                             case 2:
-                                System.out.println("Enter username to update: ");
-                                String userName = scanner.nextLine();
-                                User userToUpdate = null;
-                                for (User user : users) {
-                                    if (user.getUserName().equals(userName)) {
-                                        userToUpdate = user;
-                                        break;
-                                    }
-                                }
+                                do {
+                                    System.out.println("Enter username to update: ");
+                                    username = scanner.nextLine();
+                                } while (userController.getUser(username) == null);
 
-                                if (userToUpdate != null) {
-                                    System.out.println(updateUser(users, userToUpdate, scanner));
-                                } else {
-                                    System.out.println("User with username " + userName + " not found.");
-                                }
+                                signUpInput = signUpInput(scanner);
+
+                                User user = new User(signUpInput[0], signUpInput[1], signUpInput[2]);
+
+                                System.out.println(userController.updateUser(username, user));
+
                                 break;
                             case 3:
                                 System.out.println("Enter username to delete: ");
-                                userName = scanner.nextLine();
-                                User userToDelete = null;
-                                for (User user : users) {
-                                    if (user.getUserName().equals(userName)) {
-                                        userToDelete = user;
-                                        break;
-                                    }
-                                }
+                                username = scanner.nextLine();
 
-                                if (userToDelete != null) {
-                                    users.remove(userToDelete);
-                                    System.out.println("User deleted successfully!");
-                                } else {
-                                    System.out.println("User with username " + userName + " not found.");
-                                }
+                                System.out.println(userController.deleteUserAdmin(username));
+
                                 break;
                             case 4:
-                                if (users.isEmpty()) {
-                                    System.out.println("No users available.");
-                                } else {
-                                    for (User user : users) {
-                                        System.out.println("User ID: " + user.getUserId() + ", Username: " + user.getUserName() + ", Role: " + user.getRole());
-                                    }
+                                List<User> users = userController.getAllUsers();
+                                for (User u : users) {
+                                    System.out.println("Username: " + u.getUserName() + ", Role: " + u.getRole());
                                 }
                                 break;
                             case 5:
@@ -100,7 +92,7 @@ public class Main {
                             default:
                                 System.out.println("Invalid option. Please select a valid option.");
                         }
-                        if (taskRelated == true) {
+                        if (taskRelated) {
                             break;
                         }
                     } catch (InputMismatchException e) {
@@ -129,13 +121,13 @@ public class Main {
 
                             switch (option) {
                                 case 1:
-                                    tasks.add(createBugTask(scanner, loggedInUser.getUserName()));
+                                    taskContoller.addTask(createBugTask(scanner, loggedInUser.getUserName()));
                                     break;
                                 case 2:
-                                    tasks.add(createFeatureTask(scanner, loggedInUser.getUserName()));
+                                    taskContoller.addTask(createFeatureTask(scanner, loggedInUser.getUserName()));
                                     break;
                                 case 3:
-                                    tasks.add(createImprovementTask(scanner, loggedInUser.getUserName()));
+                                    taskContoller.addTask(createImprovementTask(scanner, loggedInUser.getUserName()));
                                     break;
                                 default:
                                     System.out.println("Invalid task type. Please try again.");
@@ -144,65 +136,44 @@ public class Main {
                         case 2:
                             System.out.println("Enter Task ID to update: ");
                             int taskId = Integer.parseInt(scanner.nextLine());
+                            Task taskToUpdate = taskContoller.getTask((long) taskId);
 
-                            Task taskToUpdate = null;
-                            for (Task task : tasks) {
-                                if (task.getId() == taskId) {
-                                    taskToUpdate = task;
-                                    break;
-                                }
-                            }
-
-                            if (taskToUpdate != null) {
-                                updateTask(taskToUpdate, scanner);
-                            } else {
+                            if (taskToUpdate == null) {
                                 System.out.println("Task with ID " + taskId + " not found.");
+                                break;
                             }
+
+                            taskContoller.updateTask(updateTask(taskToUpdate, scanner));
+                            System.out.println("Task updated successfully!");
+
                             break;
                         case 3:
                             System.out.println("Enter Task ID to delete: ");
                             taskId = Integer.parseInt(scanner.nextLine());
 
-                            Task taskToDelete = null;
-                            for (Task task : tasks) {
-                                if (task.getId() == taskId) {
-                                    taskToDelete = task;
-                                    break;
-                                }
-                            }
-
-                            if (taskToDelete != null) {
-                                tasks.remove(taskToDelete);
-                                System.out.println("Task deleted successfully!");
-                            } else {
-                                System.out.println("Task with ID " + taskId + " not found.");
-                            }
+                            System.out.println(taskContoller.deleteTask((long) taskId));
                             break;
                         case 4:
                             System.out.println("Enter Task ID to view: ");
                             taskId = Integer.parseInt(scanner.nextLine());
 
-                            Task taskToView = null;
-                            for (Task task : tasks) {
-                                if (task.getId() == taskId) {
-                                    taskToView = task;
-                                    break;
-                                }
-                            }
-
-                            if (taskToView != null) {
-                                taskToView.viewTask();
-                            } else {
+                            Task taskToView = taskContoller.getTask((long) taskId);
+                            if (taskToView == null) {
                                 System.out.println("Task with ID " + taskId + " not found.");
+                            } else {
+                                taskToView.viewTask();
                             }
                             break;
                         case 5:
-                            if (tasks.isEmpty()) {
-                                System.out.println("No tasks available.");
+                            List<Task> allTasks = taskContoller.getAllTasks();
+                            if (allTasks == null) {
+                                System.out.println("No tasks found.");
                             } else {
-                                for (Task task : tasks) {
+                                for (Task task : allTasks) {
+                                    System.out.println("--------------------");
                                     task.viewTask();
                                 }
+                                System.out.println("--------------------");
                             }
                             break;
                         case 6:
@@ -224,86 +195,29 @@ public class Main {
         }
     }
 
-    private static User createAdmin() {
-        User admin = new User();
-        admin.setUserId(1L);
-        admin.setUserName("admin");
-        admin.setPassword("admin");
-        admin.setRole("admin");
-        return admin;
-    }
-
-    private static User createUser() {
-        User user = new User();
-        user.setUserId(2L);
-        user.setUserName("user");
-        user.setPassword("user");
-        user.setRole("user");
-        return user;
-    }
-
-    private static User login(List<User> users, Scanner scanner) throws InputMismatchException {
+    private static String[] loginInput(Scanner scanner) {
         System.out.println("Enter your username: ");
         String userName = scanner.nextLine();
 
         System.out.println("Enter your password: ");
         String password = scanner.nextLine();
 
-        return authenticateUser(users, userName, password);
+        return new String[]{userName, password};
     }
 
-    private static User authenticateUser(List<User> users, String userName, String password) {
-        for (User user : users) {
-            if (user.getUserName().equals(userName) && user.getPassword().equals(password)) {
-                return user;
-            }
-        }
-        return null;
+    private static String[] signUpInput(Scanner scanner) {
+        System.out.println("Enter your username: ");
+        String userName = scanner.nextLine();
+
+        System.out.println("Enter your password: ");
+        String password = scanner.nextLine();
+
+        System.out.println("Enter your role: ");
+        String role = scanner.nextLine();
+
+        return new String[]{userName, password, role};
     }
 
-    private static User createUser(Scanner scanner, Long userId) {
-        User user = new User();
-        user.setUserId(userId);
-
-        System.out.println("Enter username: ");
-        user.setUserName(scanner.nextLine());
-
-        System.out.println("Enter password: ");
-        user.setPassword(scanner.nextLine());
-
-        System.out.println("Enter role: ");
-        user.setRole(scanner.nextLine());
-
-        return user;
-    }
-
-    private static User updateUser(List<User> users, User user, Scanner scanner) {
-        String username = "";
-        do {
-            System.out.println("Enter username: ");
-            username = scanner.nextLine();
-        } while (!isUsernameAvailable(users, username));
-
-        user.setUserName(username);
-
-        System.out.println("Enter password: ");
-        user.setPassword(scanner.nextLine());
-
-        System.out.println("Enter role: ");
-        user.setRole(scanner.nextLine());
-
-        return user;
-    }
-
-    private static boolean isUsernameAvailable(List<User> users, String username) {
-        for (User user : users) {
-            if (user.getUserName().equals(username)) {
-                System.out.println("Username already exists. Please enter a different username.");
-                return false;
-            }
-        }
-        return true;
-    }
     private static Map<String, String> createTask(Scanner scanner, String username) {
         Map<String, String> task = new HashMap<>();
 
@@ -399,7 +313,7 @@ public class Main {
         return improvementTask;
     }
 
-    private static void updateTask(Task task, Scanner scanner) {
+    private static Task updateTask(Task task, Scanner scanner) {
         System.out.println("Enter title: ");
         task.setTitle(scanner.nextLine());
 
@@ -424,6 +338,29 @@ public class Main {
 //        System.out.println("Enter created by: ");
 //        task.setCreatedBy(scanner.nextLine());
 
-        System.out.println("Task updated successfully!");
+        if (task instanceof BugTask) {
+            BugTask bugTask = (BugTask) task;
+            System.out.println("Enter severity: ");
+            bugTask.setSeverity(scanner.nextLine());
+
+            System.out.println("Enter step to reproduce: ");
+            bugTask.setStepToReproduce(scanner.nextLine());
+        } else if (task instanceof FeatureTask) {
+            FeatureTask featureTask = (FeatureTask) task;
+            System.out.println("Enter feature description: ");
+            featureTask.setFeatureDescription(scanner.nextLine());
+
+            System.out.println("Enter estimated effort: ");
+            featureTask.setEstimatedEffort(scanner.nextLine());
+        } else if (task instanceof ImprovementTask) {
+            ImprovementTask improvementTask = (ImprovementTask) task;
+            System.out.println("Enter current state: ");
+            improvementTask.setCurrentState(scanner.nextLine());
+
+            System.out.println("Enter proposed improvement: ");
+            improvementTask.setProposedImprovement(scanner.nextLine());
+        }
+
+        return task;
     }
 }
