9af376fceb3bb9dae904384b1ee1a916af4e345b
JaySabva-Ontic <jsabva@ontic.co>
2025-02-14 15:03:19 +0530

    Changes in main (to support 2 db)

--- src/main/java/org/jaysabva/SpringMain.java
+++ src/main/java/org/jaysabva/SpringMain.java
@@ -2,8 +2,16 @@ package org.jaysabva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = ElasticsearchRepository.class))
+@EnableElasticsearchRepositories(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = ElasticsearchRepository.class))
 public class SpringMain {
     public static void main(String[] args) {
         SpringApplication.run(SpringMain.class, args);
